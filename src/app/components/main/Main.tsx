3a350c309fbb99e0bc9d375fb48600336d251ca5
Nicolas Carrizo <nicolascarrizook@gmail.com>
2023-07-01 19:11:37 -0300

    update header main

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -1,11 +1,11 @@
 export default function Main() {
     return (
         <>
-            <div className="flex items-center justify-center bg_main">
-                <div className="flex flex-col items-center justify-center mx-8">
+            <div className="flex justify-center bg_main">
+                <div className="flex flex-col items-center justify-end mx-8">
                     <img src="./Texto.svg" alt="Texto" className="mb-4" />
                 </div>
-                <div className="flex flex-col items-center justify-center mx-8">
+                <div className="flex flex-col items-center justify-end mx-8">
                     <img src="./Pollo.svg" alt="Pollo" className="mb-4" />
                 </div>
             </div>
