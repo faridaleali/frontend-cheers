7d4374f248b18795f6a426df10bd5b95d31c0109
jounialsaad <jounialsaad@gmail.com>
2023-07-01 19:20:38 -0300

    about

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,7 +1,34 @@
 export default function About() {
-    return (
-        <div>
-            <img src="./Nosotros.svg" alt="Nosotros"/>
+  return (
+    <>
+      <div>
+        <div className="flex justify-center items-center p-9 m-9">
+          <img src="./Nosotros.svg" alt="Nosotros" />
         </div>
-    )
+
+        <div className="flex justify-between">
+          <div style={{ backgroundColor: "" }} className="p-9 w-1/2 m-5">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+              nobis enim quasi maiores eaque quis illum commodi eius, quas
+              beatae.
+            </p>
+          </div>
+
+          <div style={{ backgroundColor: "" }} className="p-9 w-1/2 m-5">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
+              consectetur reiciendis obcaecati odio laudantium accusantium
+              culpa. Dolores id, corrupti, ut impedit aliquam, autem ex officia
+              vero provident fugit quasi inventore.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{ backgroundColor: "yellow", width: "100vw", height: "20vh" }}
+      ></div>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/about/About";
 import MainPrincipal from "./components/mainPrincipal/MainPrincipal";
 
 export default function Home() {
   return (
     <div>
       <MainPrincipal />
+      <About />
     </div>
-  )
+  );
 }
