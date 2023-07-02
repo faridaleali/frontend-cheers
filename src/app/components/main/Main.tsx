b51b76b36d0de180d2954b5751c812e5415075a9
Nicolas Carrizo <nicolascarrizook@gmail.com>
2023-07-02 16:49:02 -0300

    update

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -3,10 +3,10 @@ export default function Main() {
         <>
             <div className="flex lg:flex-row flex-col items-center justify-center bg_main">
                 <div className="flex flex-col xs:items-center justify-end mx-8">
-                    <img src="./Texto.svg" alt="Texto" className="mb-4 xs:w-1/2 sm:w-1/3 md:w-1/4 lg:w-auto" />
+                    <img src="./Texto.svg" alt="Texto" className="mb-4 w-2/3 md:w-1/2 lg:w-auto" />
                 </div>
                 <div className="flex flex-col xs:items-center justify-end mx-8">
-                    <img src="./Pollo.svg" alt="Pollo" className="mb-4 xs:w-1/2 sm:w-1/3 md:w-1/4 lg:w-auto" />
+                    <img src="./Pollo.svg" alt="Pollo" className="mb-4 w-2/3 md:w-1/2 lg:w-auto" />
                 </div>
             </div>
         </>
