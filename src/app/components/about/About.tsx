26c8642e835cc4ea013870f2bc55e250af4f1879
Nicolas Carrizo <nicolascarrizook@gmail.com>
2023-07-02 19:37:47 -0300

    upload about section

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,34 +1,27 @@
 export default function About() {
-  return (
-    <>
-      <div>
-        <div className="flex justify-center items-center p-9 m-9">
-          <img src="./Nosotros.svg" alt="Nosotros" />
-        </div>
-
-        <div className="flex justify-between">
-          <div style={{ backgroundColor: "" }} className="p-9 w-1/2 m-5">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
-              nobis enim quasi maiores eaque quis illum commodi eius, quas
-              beatae.
-            </p>
+    return (
+      <div className="flex flex-col justify-center">
+        <div className="bg-white p-6 w-full flex flex-col items-center relative">
+          <div className="flex flex-col md:flex-row md:items-center p-9">
+            <img src="./Nosotros.svg" alt="Nosotros" />
           </div>
-
-          <div style={{ backgroundColor: "" }} className="p-9 w-1/2 m-5">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
-              consectetur reiciendis obcaecati odio laudantium accusantium
-              culpa. Dolores id, corrupti, ut impedit aliquam, autem ex officia
-              vero provident fugit quasi inventore.
-            </p>
+          <div className="flex flex-col md:flex-row text-black mb-20">
+            <div className="p-9 w-full md:w-1/2">
+              <p className="text-justify">
+                Somos una empresa dedicada exclusivamente a la venta de pollo frito, somos portadores de felicidad, perseverancia y éxito. Nuestro propósito es transmitirte las mismas experiencias sumando a seguridad, confianza y ante todo libertad para cumplir las metas que vos te propongas.
+              </p>
+            </div>
+            <div className="p-9 w-full md:w-1/2">
+              <p className="text-justify">
+                Nos enfocamos en que cada persona obtenga una experiencia distinta, todas positivas. Somos el cambio, somos creyentes de que todas las personas merecen una oportunidad. Si nosotros pudimos, vos también!
+              </p>
+            </div>
           </div>
         </div>
+        <div className="bg-[#FFFF01] h-20 w-full  flex justify-center items-center relative">
+            <img src="./pollocenter.png" alt="" className="absolute bottom-0" width={230}/>
+          </div>
       </div>
-
-      <div
-        style={{ backgroundColor: "yellow", width: "100vw", height: "20vh" }}
-      ></div>
-    </>
-  );
-}
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <MainPrincipal />
-      <About />
     </div>
   );
 }
