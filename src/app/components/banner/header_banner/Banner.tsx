899187a4187bf42790692d55150cf191faea17a7
Nicolas Carrizo <nicolascarrizook@gmail.com>
2023-07-01 21:15:29 -0300

    update page

--- src/app/components/banner/header_banner/Banner.tsx
+++ src/app/components/banner/header_banner/Banner.tsx
@@ -4,11 +4,12 @@ export default function Banner() {
             <section className="bg-banner">
                 <div className="container mx-auto px-4">
                     <div className="flex flex-wrap justify-center items-center">
-                        <div className="w-full lg:w-8/12 xs:px-4 lg:px-0">
+                        <div className="w-full lg:w-8/12 px-4">
                             <img src="./altobajon.svg" alt="" />
                         </div>
-                        <div className="w-full lg:w-4/12 xs:px-4 lg:px-0">
-                            <img src="./caja.svg" alt="" />
+                        <div className="w-full lg:w-4/12 px-4 mt-4 lg:mt-0">
+                            <img src="./caja.svg" alt="" className="hidden md:block" />
+                            <img src="./packaging.png" alt="" className="md:hidden" />
                         </div>
                     </div>
                 </div>
